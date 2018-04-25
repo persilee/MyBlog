56b8c13467cad66a3fbd69fceb95b284ee1090b8
persilee <persilee@foxmail.com>
2018-04-26 03:51:40 +0800

    Site updated: 2018-04-26 03:51:38

--- js/src/persilee-debug.js
+++ js/src/persilee-debug.js
@@ -1,1 +1,64 @@
-console.log("%c欢迎来到，李少颖的博客", " text-shadow: 0 0 5px #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
+
+POWERMODE.colorful = true; // ture 为启用礼花特效
+POWERMODE.shake = false; // false 为禁用震动特效
+document.body.addEventListener('input', POWERMODE);
+
+// 页面滚动进度条
+$(function () {
+
+  $('#header').addClass('dark');
+
+  $('.sidebar-toggle').on('click',function(){
+
+      $('#header').toggleClass('header-has-sidebar');
+
+
+  });
+
+  function scroll_fn() {
+    document_height = $(document).height();
+    scroll_so_far = $(window).scrollTop();
+    window_height = $(window).height();
+    max_scroll = document_height - window_height;
+    scroll_percentage = scroll_so_far / (max_scroll / 102);
+    $('#load').width(scroll_percentage + '%');
+    var document_width = $(document).width();
+    if (scroll_so_far && document_width >= 768) {
+      $('#header').addClass('light-header').removeClass('dark');
+    } else {
+      $('#header').removeClass('light-header').addClass('dark');
+    }
+  }
+
+  $(window).scroll(function () {
+    scroll_fn();
+  });
+
+  $(window).resize(function () {
+    scroll_fn();
+    if ($(window).width() <= 990){
+      $('#header').removeClass('header-has-sidebar');
+    } else if ($('#sidebar').width() > 0){
+      $('#header').addClass('header-has-sidebar');
+    }
+  });
+
+  setTimeout(() => {
+    $('.vhead .vname[href="https://lishaoy.net"]').after('<span class = "bozhu">博主</span>');
+  }, 500);
+
+  $('.vsubmit.vbtn').on('click',function(){
+    setTimeout(() => {
+      console.log('aaa');
+      if (!$('.vhead').eq(0).find('.vname[href="https://lishaoy.net"]~.bozhu')){
+        $('.vhead').eq(0).find('.vname[href="https://lishaoy.net"]').after('<span class = "bozhu">博主</span>');
+      }
+
+    }, 1000);
+  });
+
+
+});
+
+
+
